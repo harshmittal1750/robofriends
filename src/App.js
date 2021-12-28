33a92497952cadf0eb4b649033955ae2e436458d
Harsh Mittal <harshmittal1750@gmail.com>
2021-12-28 22:53:19 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
 import "./App.css";
-import Card from "./components/Card";
+import React, { Component } from "react";
 import CardList from "./components/CardList";
 import { robots } from "./components/robots";
+import SearchBox from "./components/SearchBox";
 
-function App() {
-  return (
-    <div className="flex">
-      <CardList robots={robots} />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      robots: robots,
+      searchfield: "",
+    };
+  }
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+    console.log(event.target.value);
+    const filteredRobots = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    console.log(filteredRobots);
+  };
+  render() {
+    const filteredRobots = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1>
+          {" "}
+          <span
+            className="f1  lh-solid  "
+            style={{ fontFamily: "Shizuru", color: "lime", fontSize: "4rem" }}
+          >
+            RoboFriends
+          </span>
+        </h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
 }
 
 export default App;
